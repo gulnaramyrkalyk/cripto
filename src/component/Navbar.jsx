f2e1d8362f1409cfc395f44714dafb80c25b58d2
gulnaramyrkalyk <myrkalykova.gulnara@gmail.com>
2023-11-24 11:01:10 +0600

    1

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from '../img/cryptocurrency.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className='logo-container'>
+            <Avatar src={icon}  size="large" />
+            <Typography.Title className='logo' level={2}>
+                <Link to='/'>Crypton</Link>
+            </Typography.Title>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
